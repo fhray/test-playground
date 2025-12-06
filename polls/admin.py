452d77b6a0d187679a4775f60e279748a1db02e3
Henning <henning.ray@gmx.de>
2025-12-06 15:40:56 -0800

    [FEATURE] Add template and views for polls

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["question_text", "pub_date"]
