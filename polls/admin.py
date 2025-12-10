76a9cb4be306f33eb0d9a078da2e1802f9d167b7
Henning <henning.ray@gmx.de>
2025-12-10 14:37:55 -0800

    [FEATURE] improve admin site

--- polls/admin.py
+++ polls/admin.py
@@ -3,9 +3,17 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 4
+
+
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ["question_text", "pub_date"]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    inlines = [ChoiceInline]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
 
 
 @admin.register(Choice)
